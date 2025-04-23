15db82e00f053a2540db8029c731561cbededfbb
R0cketM0nster <daallen89@gmailcom>
2025-04-24 09:44:56 +1200

    Updated open_webui\config.py to include FOOOCUS_API_HOST variable

--- backend/open_webui/config.py
+++ backend/open_webui/config.py
@@ -2473,6 +2473,10 @@ IMAGES_GEMINI_API_KEY = PersistentConfig(
     os.getenv("IMAGES_GEMINI_API_KEY", GEMINI_API_KEY),
 )
 
+FOOOCUS_API_HOST = os.getenv(
+    "FOOOCUS_API_HOST", "http://127.0.0.1:7865"
+)
+
 IMAGE_SIZE = PersistentConfig(
     "IMAGE_SIZE", "image_generation.size", os.getenv("IMAGE_SIZE", "512x512")
 )
